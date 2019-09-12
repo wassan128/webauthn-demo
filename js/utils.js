f9820f576d39024ababa18ee1f382859a47c8742
wassan128 <wassan.taiko.master@gmail.com>
2019-09-12 16:01:03 +0900

    Add str2bin

--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,8 @@ export function isWebAuthnSupported() {
     return !(window.PublicKeyCredential === undefined &&
         typeof window.PublicKeyCredential !== "function")
 }
+
+export function str2bin(string) {
+    return Uint8Array.from(string, c => c.charCodeAt(0))
+}
+
