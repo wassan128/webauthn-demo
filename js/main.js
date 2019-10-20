cb3e11be2267eaee6b8dea9b5d2c1320aaf9f989
wassan128 <wassan.taiko.master@gmail.com>
2019-10-20 17:54:43 +0900

    Linting

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ import { isWebAuthnSupported, str2bin } from './utils.js'
 import { Base64 } from './lib/base64.js'
 
 function getCredCreationOptions() {
-    const name = document.getElementById("name").value
+    const name = document.getElementById('name').value
     const challenge = crypto.getRandomValues(new Uint8Array(32))
-    document.getElementById("register_challenge").value = Base64.encode(challenge)
+    document.getElementById('register_challenge').value = Base64.encode(challenge)
 
     const credentialCreationOptions = {
         'challenge': challenge,
@@ -27,7 +27,7 @@ function getCredCreationOptions() {
         'attestation': 'direct'
 
     }
-    console.log("getCredCreationOptions done")
+    console.log('getCredCreationOptions done')
 
     return credentialCreationOptions
 }
@@ -43,18 +43,18 @@ async function Register() {
         String.fromCharCode(...new Uint8Array(clientDataJSON))
     )
 
-    if (clientData.type !== "webauthn.create") {
+    if (clientData.type !== 'webauthn.create') {
         console.log(`Invalid clientData type: ${clientData.type}`)
         return
     }
 
-    const challenge = document.getElementById("register_challenge").value.replace(/=/g, "")
+    const challenge = document.getElementById('register_challenge').value.replace(/=/g, '')
     if (clientData.challenge !== challenge) {
         console.log(`Incorrect clientData challenge: ${clientData.challenge} !==  ${challenge}`)
         return
     }
 
-    if (clientData.origin !== "http://localhost:8000") {
+    if (clientData.origin !== 'http://localhost:8000') {
         console.log(`Invalid clientData origin: ${clientData.origin}`)
         return
     }
@@ -64,42 +64,42 @@ async function Register() {
     const {fmt, authData, attStmt} = CBOR.decode(attestationObject)
     console.log(fmt, authData, attStmt)
 
-    const rpIdHash = authData.slice(0, 32).reduce((res, x) => res+`0${x.toString(16)}`.slice(-2), "")
-    if (rpIdHash !== sha256("localhost")) {
-        console.log(`Incorrect RP id hash not equal sha256(localhost)`)
+    const rpIdHash = authData.slice(0, 32).reduce((res, x) => res+`0${x.toString(16)}`.slice(-2), '')
+    if (rpIdHash !== sha256('localhost')) {
+        console.log('Incorrect RP id hash not equal sha256(localhost)')
     }
 
     const [uv, up] = [authData[32] & 0x04, authData[32] & 0x01]
     if (uv !== 1) {
-        console.log(`UserVerified is not 1`)
+        console.log('UserVerified is not 1')
     }
     if (up !== 1) {
-        console.log(`UserPresent is not 1`)
+        console.log('UserPresent is not 1')
     }
 
     const aaguid = authData.slice(37, 53)
     const credentialIdLength = (authData[53] << 8) + authData[54]
     const credentialId = Base64.encode(authData.slice(55, credentialIdLength))
 
-    document.getElementById("register_credentialId").value = credentialId
+    document.getElementById('register_credentialId').value = credentialId
 }
 
 function Authenticate() {
 }
 
 function sha256(target) {
-    const SHA_OBJ = new jsSHA("SHA-256", "TEXT")
+    const SHA_OBJ = new jsSHA('SHA-256', 'TEXT')
     SHA_OBJ.update(target)
-    return SHA_OBJ.getHash("HEX")
+    return SHA_OBJ.getHash('HEX')
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     if (!isWebAuthnSupported()) {
-        console.log("WebAuthn not supported... ;-(")
+        console.log('WebAuthn not supported... ;-(')
     } else {
-        console.log("WebAuthn supported :-)")
+        console.log('WebAuthn supported :-)')
     }
 
-    const btn_register = document.getElementById("btn-register")
-    btn_register.addEventListener("click", Register)
+    const btn_register = document.getElementById('btn-register')
+    btn_register.addEventListener('click', Register)
 })
