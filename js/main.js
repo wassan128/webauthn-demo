4a74b8f1075c9ff978cee1ef7675674ea5afe2b5
wassan128 <wassan.taiko.master@gmail.com>
2019-10-21 20:53:17 +0900

    Update assertion part

--- js/main.js
+++ js/main.js
@@ -88,7 +88,21 @@ async function Register() {
     console.log(publicKeyObj)
 }
 
-function Authenticate() {
+async function Authenticate() {
+    const challenge = crypto.getRandomValues(new Uint8Array(32))
+    const credentialId = document.getElementById('register_credentialId').value
+    const publicKeyCredentialRequestOptions = {
+        challenge: challenge,
+        allowCredentials: [{
+            id: Uint8Array.from(credentialId, c => c.charCodeAt(0)),
+            type: 'public-key',
+            transports: ['usb', 'ble', 'nfc'],
+        }],
+        timeout: 60000,
+    }
+    const assertion = await navigator.credentials.get({
+        publicKey: publicKeyCredentialRequestOptions
+    })
 }
 
 function sha256(target) {
